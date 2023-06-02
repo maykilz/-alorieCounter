c78c992a644a1c4f329ae4b4632f1a6ef2a1ce20
mayklz <mayil_10@mail.ru>
2023-06-03 00:30:23 +0300

    Первый коммит

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <>
+    <div className='application__header'>
+       <h1>Калькулятор колорий</h1>
+       
+    </div>
+   </> 
+  )
+}
+
+export default App;
